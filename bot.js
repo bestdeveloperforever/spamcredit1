b129f582ad8e9aa081f3a1657fb96bbb6bfb73ae
bestdeveloperforever <42047034+bestdeveloperforever@users.noreply.github.com>
2018-11-05 00:00:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ const client = new Discord.Client();
 client.on('ready', () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-client.channels.get("507997196373721096").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
-},500);
+client.channels.get("483355188954923041").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},700);
 });
 
 
